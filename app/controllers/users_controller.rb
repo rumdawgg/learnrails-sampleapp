6aeedd587ff7b3675c3c39cf4432bd7cdd5c2456
Paul Chicarello <pchicarello@patientslikeme.com>
2015-11-04 21:26:21 -0500

    sec 7.3.1 complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])    # Not the final implementation!
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
 end
