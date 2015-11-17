76238996020820cb17001cebed0302e5ec63efdd
Paul Chicarello <pchicarello@patientslikeme.com>
2015-11-17 10:57:37 -0500

    initial attempt at getting capistrano working

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,94 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'sampleapp'
+set :repo_url, 'git@github.com:rumdawgg/learnrails-sampleapp.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :scm, :git
+# Run with cap production deploy branchname=ops
+if "#{ENV['branchname']}".empty?
+  set :branch, "master"
+else
+  set :branch, "#{ENV['branchname']}"
+end
+set :currentbranch, "current"
 
-# Default value for :scm is :git
-# set :scm, :git
+# Deploy Settings
+set :deploy_user, 'www'
+set :deploy_to, "/home/#{fetch(:deploy_user)}/apps"
+set :keep_releases, 5
+set :log_level, :debug
+set :format, :pretty
+set :pty, true
+set :ssh_options, {
+  user: "#{fetch(:deploy_user)}",
+  forward_agent: true
+}
 
-# Default value for :format is :pretty
-# set :format, :pretty
+# Run db:migrate only if migrations exist
+set :conditionally_migrate, true
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# Check custom directories exist
+set :custom_directories, %W{local_shared/passenger_restart}
 
-# Default value for :pty is false
-# set :pty, true
 
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# Make sure rails and rake commands are called with bundle exec
+SSHKit.config.command_map[:rake]  = "bundle exec rake"
+SSHKit.config.command_map[:rails] = "bundle exec rails"
 
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+# Check files and directories on all servers
+namespace :environment_check do
+  desc "Check config files exist in shared/config"
+  task :config_files do
+    on roles(:app, :resque) do |host|
+      fetch(:linked_files).each do |file|
+        if test "([ -r #{fetch(:deploy_to)}/shared/#{file} ])"
+          info "#{fetch(:deploy_to)}/shared/#{file} exists on #{host}"
+        else
+          error "#{fetch(:deploy_to)}/shared/#{file} do not exist on #{host}"
+        end
+      end
     end
   end
 
+  desc "Check if custom directories exist"
+  task :custom_directories do
+    on roles(:app, :resque) do |host|
+      fetch(:custom_directories).each do |dir|
+        if test "([ -d #{fetch(:deploy_to)}/#{dir} ])"
+          info "#{fetch(:deploy_to)}/#{dir} exists on #{host}"
+        else
+          execute :mkdir, '-p', "#{fetch(:deploy_to)}/#{dir}"
+        end
+      end
+    end
+  end
+
+  desc "Run all environment check tasks"
+  task :all do
+    invoke 'environment_check:config_files'
+    invoke 'environment_check:custom_directories'
+  end
+end
+
+# Deploy tasks
+namespace :deploy do
+    desc "Restart application using a rolling restart"
+    task :rolling_restart do
+        on roles(:app), in: :sequence, wait: 10 do |host|
+            on roles(:web) do
+                set_haproxy_state('disable', "#{host}")
+            end 
+            sleep(10)
+            execute :touch, "#{fetch(:deploy_to)}/local_shared/passenger_restart/restart.txt"
+            sleep(10)
+            on roles(:web) do
+                set_haproxy_state('enable', "#{host}")
+            end
+        end
+    end
+
+    before :deploy, 'environment_check:all'
+    after :publishing, :restart
 end
