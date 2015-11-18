4e51921f8b7e004f0d1c58d83bd82139ac3b3c0f
Paul Chicarello <pchicarello@patientslikeme.com>
2015-11-17 20:54:13 -0500

    preparing for local env to send mail through sendgrid

--- config/environments/local.rb
+++ config/environments/local.rb
@@ -21,9 +21,9 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :user_name      => ENV['sendgrid_user'],
+    :password       => ENV['sendgrid_pass'],
+    :domain         => 'chicarello.com',
     :enable_starttls_auto => true
   }
 end
